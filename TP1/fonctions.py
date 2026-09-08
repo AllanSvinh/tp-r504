1fe1223673e495f0707360f5324cc15f48efaf40
Allan <allansvinh@gmail.com>
2026-09-08 17:12:07 +0200

    changement de l'utilisation de **

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,19 @@
 def puissance(nombre_a, nombre_b):
-    if not type(nombre_a) is int:
-        raise TypeError("Only integers are allowed")
+	if not type(nombre_a) is int:
+		raise TypeError("Only integers are allowed")
     
-    if not type(nombre_b) is int:
-        raise TypeError("Only integers are allowed")
+	if not type(nombre_b) is int:
+		raise TypeError("Only integers are allowed")
 
-    if nombre_a == 0 and nombre_b < 0:
-        raise ValueError("0 élevé à une puissance négative est indéfini")
-        
-    return nombre_a ** nombre_b
+	if nombre_a == 0 and nombre_b < 0:
+		raise ValueError("0 élevé à une puissance négative est indéfini")
+
+	result = 1
+
+	for i in range(nombre_b):
+		result=result*nombre_a
+
+	if nombre_b < 0:
+		return 1 / result
+
+	return result
